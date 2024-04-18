4752f109ddbd98f1e3b3a16cfba670cdcf732fb1
Javokhir1105 <161455530+Javokhir1105@users.noreply.github.com>
2024-04-18 18:15:48 +0500

    js qo'shildi

---
+++ index.js
@@ -0,0 +1,79 @@
+// let x=Number(prompt("x sonni kiriting"))
+// let summ=""
+// let a=" "
+// for(i=1; i<=x; i++){
+//     summ+=i+a
+//     console.log(summ)
+// }
+
+
+// let x=prompt("x soni")-0;
+// let y=prompt("y soni")-0;
+// let sum=0;
+// let a=1;
+// let b=(x%2==0)*2
+//  for(let i=b; i<=y; i++){
+//         sum+=i;
+//         a*=i;
+//     }
+//     console.log(sum,a,(sum+a))
+
+// 1-misol
+// let a=7;
+// let b=50
+// for(i=a+1; i<=b; i+=2){
+//     console.log(i)
+// }
+// 2-misol
+// let a=100
+// let summ=0
+// for(i=23; i<=a; i+=2){
+//     summ+=i
+// }
+// console.log(summ)
+// 3-misol
+// for(i=21; i<=200; i++){
+//     if(i%5==0){
+//         console.log(i)
+//     }
+// }
+// 4-misol
+// let summ=0
+// for(i=1; i<=50; i++){
+//     if(i>=20){
+//         summ+=i
+//     }
+// }
+// console.log(summ)
+// 5-misol
+// let kupaytma=1
+// for(i=11; i<=39; i++){
+//     if(i%7==0){
+//         kupaytma*=i
+//     }
+// }
+// console.log(kupaytma)
+// 6-misol
+// for(i=1000; i<2000; i++){
+//     if(i%13==0){
+//         console.log(i)
+        
+//         break;
+//     }
+// }
+// 7-misol
+// for(i=8; i<=100; i++){
+//     if(i%7==0 && i%11==0){
+//         console.log(i)
+//     }
+// }
+// 8-misol
+// let summ=0
+// let kupaytma=1
+// let ayirma=0
+// for(i=16; i<=100; i+=2){
+//     summ+=i
+//     kupaytma*=i
+// }
+// ayirma=kupaytma-summ
+// console.log(ayirma+" ayirmasi")
